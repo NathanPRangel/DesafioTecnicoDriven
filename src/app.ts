22686be71d3a9e41304acef3b1f554a7afb88d19
Nathan Rangel <nathanpk20@gmail.com>
2023-11-12 13:06:18 -0300

    modificando schema

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import participantRouter from './routers/participants-routers';
+import gamesRouter from './routers/games-routers';
 
 const app = express();
-export default app;
 
 app.use(json());
 app.use(cors());
 app.use('/participant', participantRouter);
+app.use('/games', gamesRouter);
+
+export default app;
